6a1ccbcfe5dbad7d389fbc44a8dd4cb3088a501d
Pedro Almir <petrus.cc@gmail.com>
2013-06-30 17:25:38 -0300

    GAE initial project

---
+++ src/com/pedroalmir/Athena_Servlet.java
@@ -0,0 +1,11 @@
+package com.pedroalmir;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Athena_Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
